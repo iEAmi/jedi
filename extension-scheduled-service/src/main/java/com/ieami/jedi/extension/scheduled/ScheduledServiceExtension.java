0b834fefc02d031ad4bd89996bf628a5d90bb708
iEAmi <EAhmad.edge@gmail.com>
2022-08-21 18:48:04 +0430

    Implementing Scheduler extension

---
+++ extension-scheduled-service/src/main/java/com/ieami/jedi/extension/scheduled/ScheduledServiceExtension.java
@@ -0,0 +1,12 @@
+package com.ieami.jedi.extension.scheduled;
+
+import com.ieami.jedi.core.DependencyCollection;
+import com.ieami.jedi.core.Extension;
+import org.jetbrains.annotations.NotNull;
+
+public final class ScheduledServiceExtension extends Extension {
+
+    public ScheduledServiceExtension(@NotNull DependencyCollection dependencyCollection) {
+        super(dependencyCollection);
+    }
+}
